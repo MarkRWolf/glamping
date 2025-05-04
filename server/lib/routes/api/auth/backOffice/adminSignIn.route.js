c4a15ca01720ae7fbf06a0a1d40a43466c33413b
Mark W <MarkRWolf@gmail.com>
2025-05-04 19:27:45 +0200

    cookie

--- server/lib/routes/api/auth/backOffice/adminSignIn.route.js
+++ server/lib/routes/api/auth/backOffice/adminSignIn.route.js
@@ -29,7 +29,7 @@ adminSignIn.post("/auth/adminSignIn", async (req, res) => {
     res.cookie("glampingToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).send({ message: "Successfully signed in" });
